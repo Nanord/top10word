36b7ca92b123ed332a151d01714b500cb51a8328
Maxim Panov <maksim.panov@stm-labs.ru>
2021-04-27 18:27:35 +0300

    тэсты

---
+++ src/test/java/com/stm/top10word/service/worker/impl/FilterWordMapperQueueWorkerServiceImplTest.java
@@ -0,0 +1,74 @@
+package com.stm.top10word.service.worker.impl;
+
+import com.stm.top10word.configuration.ExecutorConfiguration;
+import com.stm.top10word.service.filters.word.WordFilterService;
+import com.stm.top10word.service.filters.word.impl.IsNotEmptyFilterServiceImpl;
+import com.stm.top10word.service.filters.word.impl.IsNotNumericFilterServiceImpl;
+import com.stm.top10word.service.filters.word.impl.WordLengthFilterServiceImpl;
+import com.stm.top10word.utils.TestUtils;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import static org.junit.Assert.assertEquals;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class FilterWordMapperQueueWorkerServiceImplTest {
+
+    private ThreadPoolTaskExecutor threadPoolTaskExecutor;
+
+    private final String stopWord = "STOPWORD";
+
+    @Before
+    public void prepare() {
+        ExecutorConfiguration executorConfiguration = new ExecutorConfiguration();
+        ReflectionTestUtils.setField(executorConfiguration, "threadPoolSizeReader", 10);
+        threadPoolTaskExecutor = executorConfiguration.threadPoolTaskExecutorStarter();
+        threadPoolTaskExecutor.initialize();
+    }
+
+    @Test
+    public void workTest() {
+
+        List<WordFilterService> wordFilterServiceList = new ArrayList<>();
+        wordFilterServiceList.add(new IsNotEmptyFilterServiceImpl());
+        wordFilterServiceList.add(new IsNotNumericFilterServiceImpl());
+        WordLengthFilterServiceImpl wordLengthFilterService = new WordLengthFilterServiceImpl();
+        ReflectionTestUtils.setField(wordLengthFilterService, "wordLength", 5);
+        wordFilterServiceList.add(wordLengthFilterService);
+        FilterWordMapperQueueWorkerServiceImpl filterWordMapperQueueWorkerService =
+                new FilterWordMapperQueueWorkerServiceImpl(threadPoolTaskExecutor, wordFilterServiceList);
+        ReflectionTestUtils.setField(filterWordMapperQueueWorkerService, "queueSize", 3);
+
+        BlockingQueue<String> inputQueue = new LinkedBlockingQueue<>();
+
+        inputQueue.offer("one");
+        inputQueue.offer("one");
+        inputQueue.offer("verilongword");
+        inputQueue.offer("three");
+        inputQueue.offer("four");
+        inputQueue.offer("five");
+        inputQueue.offer("longword");
+        inputQueue.offer("longword");
+        inputQueue.offer(stopWord);
+
+        BlockingQueue<String> qResult = filterWordMapperQueueWorkerService.doWork(stopWord, stopWord , inputQueue);
+
+        Map<String, Integer> result = new HashMap<>();
+        TestUtils.queueWatcher(result, qResult, stopWord);
+        assertEquals(2, result.get("longword").intValue());
+
+    }
+}
